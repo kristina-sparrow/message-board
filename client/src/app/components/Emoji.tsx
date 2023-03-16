b3b4fa79e942d2cdb644d24d33d2779f3d2a2a61
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-03-16 10:33:00 -0600

    Create emoji picker component

---
+++ client/src/app/components/Emoji.tsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+import { BsEmojiSmile } from "react-icons/bs";
+import useOnClickOutside from "../hooks/useOnClickOutside";
+
+type EmojiProps = {
+  setEmoji: React.Dispatch<React.SetStateAction<any>>;
+};
+
+export default function Emoji({ setEmoji }: EmojiProps) {
+  const [isPickerVisible, setIsPickerVisible] = useState(false);
+  const emojiPickerRef = useRef<HTMLDivElement>(null);
+
+  const handlePicker = () => setIsPickerVisible(!isPickerVisible);
+
+  useOnClickOutside(emojiPickerRef, () => handlePicker());
+
+  return (
+    <div className="emoji">
+      {isPickerVisible && (
+        <div ref={emojiPickerRef} className="picker">
+          <EmojiPicker
+            onEmojiClick={(e) => {
+              setEmoji(e.emoji);
+              handlePicker();
+            }}
+          />
+        </div>
+      )}
+      <button type="button" onClick={() => handlePicker()}>
+        <BsEmojiSmile />
+      </button>
+    </div>
+  );
+}
