d8805f707162c3286117b3a7ef836557c74946d6
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-03-16 09:11:09 -0600

    Create basic frontend layout

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,1 +1,10 @@
-import "index.scss";
+import "./index.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
