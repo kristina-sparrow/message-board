f0de47e24f91bb8325ce03be338d933d89007ab2
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-03-16 11:34:12 -0600

    Add main logic and infinite scroll

--- client/src/app/components/Main.tsx
+++ client/src/app/components/Main.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Card from "./Card";
 import MessageBoard from "./MessageBoard";
+import SendMessage from "./SendMessage";
+import { MessageProps, getMessage } from "../services/message";
+import useInfiniteScroll from "../hooks/useInfiniteScroll";
 
 export default function Main() {
+  const [messages, setMessages] = useState<MessageProps[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [last, setLast] = useState(false);
+  const { loadMoreRef, skip } = useInfiniteScroll();
+
+  const fetchMessages = useCallback(async () => {
+    try {
+      setLoading(true);
+      if (!last) {
+        getMessage(skip).then((data) => {
+          setMessages((prev) => [...data.reverse(), ...prev]);
+          if (data.length < 10 && data.length > 0) {
+            setLast(true);
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [skip]);
+
+  useEffect(() => {
+    fetchMessages();
+  }, [fetchMessages]);
+
   return (
     <main className="main">
       <Card>
@@ -11,6 +42,7 @@ export default function Main() {
           loadMoreRef={loadMoreRef}
           last={last}
         />
+        <SendMessage setMessages={setMessages} />
       </Card>
     </main>
   );
---
+++ client/src/app/hooks/useInfiniteScroll.ts
@@ -0,0 +1,27 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+export default function useInfiniteScroll() {
+  const [skip, setSkip] = useState(0);
+  const loadMoreRef = useRef<HTMLDivElement>(null);
+
+  const handleObserver = useCallback((entries: any) => {
+    const [target] = entries;
+    if (target.isIntersecting) {
+      setSkip((a) => a + 10);
+    }
+  }, []);
+
+  useEffect(() => {
+    const option: IntersectionObserverInit = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+
+    if (loadMoreRef.current) observer.observe(loadMoreRef.current);
+  }, [handleObserver]);
+
+  return { loadMoreRef, skip };
+}
