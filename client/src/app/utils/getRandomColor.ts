581906864c793d919c1f8b0840d3fd2d50b23d31
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-03-19 18:32:22 -0600

    Design tweaks

--- client/src/app/utils/getRandomColor.ts
+++ client/src/app/utils/getRandomColor.ts
@@ -1,4 +1,14 @@
-const palette = ["#FACCAA", "#A1BCE7", "#9ED9B3", "#DAB3B4", "#AAA2C9"];
+const palette = [
+  "#EAE4E9",
+  "#FFF1E6",
+  "#FDE2E4",
+  "#FAD2E1",
+  "#E2ECE9",
+  "#BEE1E6",
+  "#F0EFEB",
+  "#DFE7FD",
+  "#CDDAFD",
+];
 
 export default function getRandomColor() {
   const randomIndex = Math.floor(Math.random() * palette.length);
